6f0669e4dd7b34fffb1c0602e31803f70f63d34b
Yordan Saldaña <yordansaldanabarrera@gmail.com>
2024-09-04 16:33:05 -0300

    add Font

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./styles/index.css";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "Centra, sans-serif", // Aplica la fuente a todos los textos de MUI
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
