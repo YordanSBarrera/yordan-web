151e58d47fede7a2dc1c60ca85ff5bc7f6d448be
Yordan Saldaña <yordansaldanabarrera@gmail.com>
2025-01-31 09:55:22 -0300

    add Responsive FigureAnimation

--- src/components/FigureAnimation.tsx
+++ src/components/FigureAnimation.tsx
@@ -5,15 +5,20 @@ import { Box, Stack } from "@mui/material";
 
 const FigureAnimation = () => {
   return (
-    <Stack className="figure">
+    <Stack className="figure" width="100%">
       <TrackVisibility>
         {({ isVisible }) => (
           <Box
             className={
               isVisible ? "animate__animated animate__zoomInRight" : ""
             }
+            sx={{
+              width: { xs: "100%", sm: "80%", md: "100%", lg: "600px" },
+              margin: "0 auto",
+              padding: { xs: "10px", sm: "20px", md: "30px" },
+            }}
           >
-            <img src={AstronautFigure} alt="Header Img" width={"600px"} />
+            <img src={AstronautFigure} alt="Header Img" width="100%" />
           </Box>
         )}
       </TrackVisibility>
