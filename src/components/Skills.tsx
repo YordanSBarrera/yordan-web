c7b2a1fec43f910fd562df456f350bd3f97a2d48
Yordan Saldaña <yordansaldanabarrera@gmail.com>
2025-02-09 12:54:04 -0300

    Update Skills.tsx

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,7 +1,7 @@
 import { Stack, Typography } from "@mui/material";
 import CardTransparent from "./CardTransparent";
+import { useTranslation } from "react-i18next";
 
-import git from "../assets/icons/1/git.svg";
 import github from "../assets/icons/1/github.svg";
 import bootstrap from "../assets/icons/1/bootstrap-oreja.svg";
 import css from "../assets/icons/1/css.svg";
@@ -11,25 +11,14 @@ import react from "../assets/icons/1/reaccionar.svg";
 import vsc from "../assets/icons/1/vsc2019.svg";
 import vite from "../assets/icons/1/vite.svg";
 import ts from "../assets/icons/1/ts.svg";
-import mongo from "../assets/icons/1/mongodb.svg";
-import mui from "../assets/icons/1/material-ui.svg";
 import angular from "../assets/icons/1/angular.svg";
-import emailW from "../assets/icons/1/email-white.svg";
-import githubW from "../assets/icons/1/github-white.svg";
-import linkedinW from "../assets/icons/1/linkedin-white.svg";
-import { useTranslation } from "react-i18next";
-
 import gitNew from "../assets/icons/git.svg";
-import gitHubDark from "../assets/icons/Github_dark.svg";
-import gitHubLigth from "../assets/icons/Github_light.svg";
 import mongoDb from "../assets/icons/mongodb.svg";
 import npm from "../assets/icons/npm.svg";
 import yarn from "../assets/icons/yarn.svg";
-import pnpmDark from "../assets/icons/Pnpm_dark.svg";
 import pnpmLigth from "../assets/icons/Pnpm_light.svg";
 import next from "../assets/icons/nextjs_icon_dark.svg";
 import postman from "../assets/icons/postman.svg";
-import javascript from "../assets/icons/javascript.svg";
 import json from "../assets/icons/json-schema.svg";
 import reactQuery from "../assets/icons/reactquery.svg";
 import reactRouter from "../assets/icons/reactrouter.svg";
