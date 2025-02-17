50c4169f104cfb8a6dbf08acd9bb125c7dd41708
Yordan Saldaña <yordansaldanabarrera@gmail.com>
2025-02-17 09:51:08 -0300

    add Dashboard new Project card

--- src/modules/Project/Projects.tsx
+++ src/modules/Project/Projects.tsx
@@ -5,6 +5,7 @@ import ProjectGridCard from "./ProjectGridCard";
 import medisproutImg from "../../assets/img/calendarMedisprout.webp";
 import ecomerceImg from "../../assets/img/project-eCommerce500x320.webp";
 import usersList from "../../assets/img/usersList.webp";
+import dashboard from "../../assets/img/dashboard.webp";
 
 const Projects = () => {
   const { t } = useTranslation();
@@ -34,10 +35,10 @@ const Projects = () => {
           projectDescription={t("usersListDetails")}
         />
         <ProjectGridCard
-          cardName={t("medisprout")}
-          imgSrc={medisproutImg}
-          imgAlt={t("medisproutAppImg")}
-          projectDescription={t("medisproutDetail")}
+          cardName={t("dashboard")}
+          imgSrc={dashboard}
+          imgAlt={t("dashboardAppImg")}
+          projectDescription={t("dashboardDetail")}
         />
       </Grid>
     </Box>
